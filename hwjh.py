0585d3abb824fba3fc46eca43ab3e93685ccd5a1
hjiang-dl <74637995+hjiang-dl@users.noreply.github.com>
2021-03-10 06:46:39 +0800

    Update hwjh.py

--- hwjh.py
+++ hwjh.py
@@ -1,19 +1,21 @@
-import requests
+import requests
 
-def get_information(username):
-    list1 = list()
-    r = requests.get(f'https://api.github.com/users/{username}/repos')
-    json = r.json()
-    for  p  in  range ( 0 , len ( json )):
-        rname = json[p]['name']
-        commit = requests.get(f'https://api.github.com/repos/{username}/{rname}/commits')
-        c = commit.json()
-        list1.append(f"Repo: {rname} Commits number: {len(c)}")
-    return list1
+def get_information(username):
+    list1 = list()
+    f='https://api.github.com/users/'+{username}+'/repos'
+    r = requests.get(f)
+    json = r.json()
+    for  p  in  range ( 0 , len ( json )):
+        rname = json[p]['name']
+        f='https://api.github.com/repos/'+{username}+'/'+{rname}+'/commits'
+        commit = requests.get(f)
+        c = commit.json()
+        list1.append(f"Repo: {rname} Commits number: {len(c)}")
+    return list1
 
-def main():
-    username = input("Please enter the username:")
-    print(get_information(username))
+def main():
+    username = input("Please enter the username:")
+    print(get_information(username))
 
-if __name__=='__main__':
-    main()
+if __name__=='__main__':
+    main()
