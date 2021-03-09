93cf9df179d3f65eea6b42048ca498852ec626be
hjiang-dl <74637995+hjiang-dl@users.noreply.github.com>
2021-03-10 07:07:07 +0800

    Update hwjh.py

--- hwjh.py
+++ hwjh.py
@@ -2,12 +2,12 @@ import requests
 
 def get_information(username):
     list1 = list()
-    f='https://api.github.com/users/'+{username}+'/repos'
+    f='https://api.github.com/users/'+{username}+'/repos'
     r = requests.get(f)
     json = r.json()
     for  p  in  range ( 0 , len ( json )):
         rname = json[p]['name']
-        f='https://api.github.com/repos/'+{username}+'/'+{rname}+'/commits'
+        f='https://api.github.com/repos/'+{username}+'/'+{rname}+'/commits'
         commit = requests.get(f)
         c = commit.json()
         list1.append(f"Repo: {rname} Commits number: {len(c)}")
